4b6796d3b63eb9fd3ed09768243a58075c0b3544
Perrels <pablorela@gmail.com>
2024-06-13 13:54:17 -0300

    feat: finished the 1st exercise (Introduction to numPy)

--- PROJETOS/1/exercicio1Numpy.py
+++ PROJETOS/1/exercicio1Numpy.py
@@ -4,14 +4,21 @@ lista = []
 for i in range(1, 4):
     while True:
         try:
-            x = input(f"insira o {i} número \n")
+            # passando valores com parse para float senão da problema
+            x = float(input(f"insira o {i} número \n"))
             lista.append(x)
             break
         except ValueError:
             print("Entrada inválida. Por favor, insira um número válido")
-            
-            
+
+
 # transformando a lista num npArray
 numPyArray = np.array(lista)
-
-#
+print(f'Lista do array do numPy{numPyArray}')
+print("----------------------------------------\n")
+# crie maximo, minimo, menor posicao, desvio padrao e media
+print("Máx", numPyArray.max())
+print("Min", numPyArray.min())
+print("Menor posicao", numPyArray.argmax())
+print("Desvio padrão", numPyArray.std())
+print("Média", numPyArray.mean())
